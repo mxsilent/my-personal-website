3cd43cf13b13e179b4a00b70766a54a502c34a9d
Matthew Starks <mstark_558@hotmail.com>
2022-11-07 15:16:36 -0600

    pushing minor section update & 'coming soon' messaging

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('home').addEventListener("click", function(){
+    alert("Coming Soon");
+});
+
+document.getElementById('about').addEventListener("click", function(){
+    alert("Coming Soon");
+});
+
+document.getElementById('services').addEventListener("click", function(){
+    alert("Coming Soon");
+});
+
+document.getElementById('skills').addEventListener("click", function(){
+    alert("Coming Soon");
+});
